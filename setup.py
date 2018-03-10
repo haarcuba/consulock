6d8feca35451f4f8e8be99d85579060db7f9ebfe
Yoav Kleinberger <haarcuba@gmail.com>
2018-03-11 01:34:37 +0200

    version 0.2.1

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ tests_require = [
 requires = [ 'python-consul', ]
 
 setup(name='consulock',
-      version='0.2.0',
+      version='0.2.1',
       description=README,
       long_description=README,
       url='https://github.com/haarcuba/consulock',
